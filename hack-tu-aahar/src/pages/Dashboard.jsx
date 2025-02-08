7638e95918cc57a5aa883d714b6bae15b12520e4
Innisfree-11 <rogersdivyam@gmail.com>
2025-02-08 19:15:24 +0530

    add new dependencies and enhance Dashboard layout with improved weather display

--- hack-tu-aahar/src/pages/Dashboard.jsx
+++ hack-tu-aahar/src/pages/Dashboard.jsx
@@ -1,10 +1,14 @@
+import { Thermometer } from "lucide-react";
 function Dashboard() {
   return (
     <div>
       <div className="grid grid-cols-3 gap-4">
-        <div className="bg-red-500 col-span-1 flex justify-evenly">
-          <div className="font-roboto font-semibold">Weather</div>
-          <div className="font-roboto font-semibold">27C</div>
+        <div className="bg-red-500 col-span-1">
+          <div className=" flex justify-evenly">
+            <div className="font-roboto font-semibold">Weather</div>
+            <div className="font-roboto font-semibold">27C</div>
+          </div>
+          div.
         </div>
         <div className="bg-blue-500 col-span-2">hi</div>
       </div>
