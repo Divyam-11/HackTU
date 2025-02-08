a8e8a5e635a0d30eb219c96c7990330c27fff37c
Innisfree-11 <rogersdivyam@gmail.com>
2025-02-08 17:15:24 +0530

    enhance Dashboard component layout with weather display and grid styling

--- hack-tu-aahar/src/pages/Dashboard.jsx
+++ hack-tu-aahar/src/pages/Dashboard.jsx
@@ -1,7 +1,13 @@
 function Dashboard() {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="bg-red-500 col-span-1 flex justify-evenly">
+          <div className="font-roboto font-semibold">Weather</div>
+          <div className="font-roboto font-semibold">27C</div>
+        </div>
+        <div className="bg-blue-500 col-span-2">hi</div>
+      </div>
     </div>
   );
 }
